0c2e605e4451d4d2c1abb046e6f070bef5d710ca
Wix Remote Machine <support@wix.com>
2026-01-29 15:05:16 +0000

    Manual code changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ export default function Header() {
               Home
             </Link>
             <Link 
-              to="/store" 
+              to="#hampers" 
               className="font-paragraph text-base text-foreground hover:text-primary transition-colors"
             >
               Shop
